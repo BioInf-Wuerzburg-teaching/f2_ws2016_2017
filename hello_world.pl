aada8a96a1c040b675e1bfdadf99efba2612d2c6
Mel Sch <melsch@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-26 10:34:28 +0200

    Variablen-Problem bearbeitet

--- hello_world.pl
+++ hello_world.pl
@@ -1,5 +1,19 @@
+use warnings;
 print "Hello World\n", $bla;
 print 5, "\n";
 print 5+5, "\n";
 $bla="Hello World2\n";
 print $bla;
+
+$bla=5;
+$blub="3Hallo";
+$sum=$bla+$blub;
+print $sum, "\n";
+
+$eins=2;
+$zwei=1;
+
+$speicher=$eins;
+$eins=$zwei;
+$zwei=$speicher;
+print "$eins,$zwei\n"
