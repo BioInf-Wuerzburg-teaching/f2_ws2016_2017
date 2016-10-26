439560dd0a5b7e0c39cc0aab0efd482ca28903f1
Florens Fischer <flofis@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-26 10:30:32 +0200

    Hallo Welt! Rechnung und Problem eins,zwei

--- helloworld.pl
+++ helloworld.pl
@@ -1,2 +1,22 @@
+use warnings;
+print "Hallo\n";
 $bla="Guten Apetit!\n";
-print "Hallo Welt!","\n","\t",5+5,"\n",$bla;
+print 5,"\n";
+print $bla,"\n";
+
+$bla=5;
+$blub=3;
+$sum=$bla+$blub;
+
+print $sum,"\n","vorher","\n";
+
+$eins=2;
+$zwei=1;
+print "eins:",$eins,"\n","zwei:",$zwei,"\n","\n";
+
+$speicher=$eins;
+$eins=$zwei;
+$zwei=$speicher;
+print "nachher","\n","eins:",$eins,"\n","zwei:",$zwei,"\n";
+
+
