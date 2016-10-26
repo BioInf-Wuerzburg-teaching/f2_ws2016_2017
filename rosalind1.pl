6ea4a78120062995b6fe24bb0cb240fa3471dcdb
Florens Fischer <flofis@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-26 17:35:36 +0200

    rosalind1

---
+++ rosalind1.pl
@@ -0,0 +1,30 @@
+use warnings;
+
+
+$dna='AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC';
+@bases=split(//,$dna);
+
+
+$ende=@bases;
+$A=0;
+$C=0;
+$G=0;
+$T=0;
+for($i=0;$i<$ende;$i++)
+{
+    if($bases[$i] eq 'A')
+    {$A++}
+
+    
+    if($bases[$i] eq 'C')
+    {$C++}
+
+    if($bases[$i] eq 'G')
+    {$G++}
+    
+    if($bases[$i] eq 'T')
+    {$T++}
+}
+print "$A,$C,$G,$T\n";
+
+
