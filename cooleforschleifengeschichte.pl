fbcc7f73d11d8443e5ad0eca9c0ccf7a3aaf768d
Anna-Sophie Schaller <Obsivergate@web.de>
2016-10-26 17:48:18 +0200

    rosalindmitpearl

---
+++ cooleforschleifengeschichte.pl
@@ -0,0 +1,16 @@
+use warnings;
+
+
+#for($i=10;$i>0;$i--)
+#{
+#   print "$i\n";
+#    sleep 5;
+#}
+
+@willi=('a','b','c','d');
+
+$ende=@willi;
+for($i=0;$i<$ende;$i++)
+{
+    print$willi[$i],"\n";
+}
