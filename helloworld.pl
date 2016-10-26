62a4db8dca5c55ac1bb1a35b4555d28f1ab7bf9d
Florens Fischer <flofis@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-26 11:00:04 +0200

    Hallo Welt! Rechnung und Problem eins,zwei; Willi

--- helloworld.pl
+++ helloworld.pl
@@ -1,22 +1,38 @@
 use warnings;
 print "Hallo\n";
 $bla="Guten Apetit!\n";
-print 5,"\n";
+#print 5,"\n";
 print $bla,"\n";
 
-$bla=5;
-$blub=3;
-$sum=$bla+$blub;
+#$bla=5;
+#$blub=3;
+#$sum=$bla+$blub;
 
-print $sum,"\n","vorher","\n";
+#print $sum,"\n\nvorher\n";
 
 $eins=2;
 $zwei=1;
-print "eins:",$eins,"\n","zwei:",$zwei,"\n","\n";
+print "eins:$eins\nzwei:$zwei\n\n";
 
 $speicher=$eins;
 $eins=$zwei;
 $zwei=$speicher;
-print "nachher","\n","eins:",$eins,"\n","zwei:",$zwei,"\n";
+print "nachher\neins:$eins\nzwei:$zwei\n\n\n";
+
+$willi="Blume";
+$maja="Gras";
+
+print "Wer?\t\tWo?\n";
+print "Willi:\t\t$willi\nMaja:\t\t$maja\n\n";
+
+$zettelchen="leer\n";
+print "Zettelchen:\t$zettelchen\n";
+
+$zettelchen=$willi;
+$willi=$maja;
+$maja=$zettelchen;
+
+
+print "Zettelchen:\t$zettelchen\n\nWilli:\t\t$willi\nMaja:\t\t$maja\n\n";
 
 
