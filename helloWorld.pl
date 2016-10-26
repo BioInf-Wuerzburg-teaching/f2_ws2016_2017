dbfb05f48dea85e24862c4a60dec4321cda961ea
Jocelyn Orf <jocorf@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-26 10:38:51 +0200

    hello world, variablen, variablen tauschen

--- helloWorld.pl
+++ helloWorld.pl
@@ -1,5 +1,18 @@
+#use warnings;
 print "Hello World!\n",$bla;
 print 5,"\n";
 print 5+5,"\n";
 $bla="hello world";
 print $bla,"\n";
+
+$bla=5;
+$blub="3Hallo";
+$sum=$bla+$blub;
+print $sum, "\n";
+
+$eins="B";
+$zwei="A";
+$speich=$eins;
+$eins=$zwei;
+$zwei=$speich;
+print "$eins,$zwei\n";
