16907d8b3a64519594860f40887bd382040d531e
Mel Sch <melsch@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-27 14:24:26 +0200

    Sortierproblem geloest

--- rosalind1_hash.pl
+++ rosalind1_hash.pl
@@ -1,5 +1,5 @@
 use warnings;
-$dna='AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC';
+$dna='AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTZCTGATAGCAGC';
 @bases=split(//,$dna);
 $l=@bases;
 %hash=();
@@ -11,9 +11,26 @@ for($i=0;$i<$l;$i++)
     
     
 }
+
+if (exists $hash{Z})
+{
+    print "Z ist da!\n";
+}
+
+print "A ", $hash{A}, "\n";
+
 foreach $schluessel (sort keys %hash)
 {
-    
-    print "$schluessel ";
-    print $hash{$schluessel}, "\n";
+    if ($schluessel eq "A")
+    {
+	print "$schluessel ";
+	print $hash{$schluessel}, "\n";
+    }
 }
+
+# @Array=(3,7,5,1,46,18,225,4,42,5);
+# print "@Array", "\n";
+# @ArrayS=sort(@Array);
+# print "@ArrayS", "\n";
+# @ArrayS2=sort{$b <=> $a}(@Array);
+# print "@ArrayS2\n";
