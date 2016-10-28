65fb94a3441c3a782c6eafbaef0f11047ae3fdf4
Anna-Sophie Schaller <Obsivergate@web.de>
2016-10-28 10:47:09 +0200

    Quersummen mehrerer Zahlen sortiert

--- quersumme.pl
+++ quersumme.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-my $zahl='123456';
+my $zahl=123456;
 my @ziffer=split(//,$zahl);
 my $l=@ziffer;
 my $erg=0;
@@ -13,4 +13,30 @@ for(my $i=0;$i<$l;$i++)
 
 print "$erg\n";
 
+my @num=(115,2004,3526,99999,80023,850,356,1420,000001,999999);
+print "@num\n";
 
+@num=sort{
+
+my $zahl=$a;
+my @ziffer=split(//,$zahl);
+my $l=@ziffer;
+my $erg=0;
+
+for(my $i=0;$i<$l;$i++)
+{
+    $erg = $erg+$ziffer[$i];
+}
+$zahl=$b;
+@ziffer=split(//,$zahl);
+$l=@ziffer;
+my $ergb=0;
+
+for(my $i=0;$i<$l;$i++)
+{
+    $ergb = $ergb+$ziffer[$i];
+}
+
+$erg<=>$ergb;
+}(@num);
+print"@num\n";
