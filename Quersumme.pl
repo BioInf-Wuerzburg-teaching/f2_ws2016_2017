9161459071616ed1e33af90d7c708835499af7af
Mel Sch <melsch@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-28 12:21:54 +0200

    Quersumme sortiert mittels Funktionen

--- Quersumme.pl
+++ Quersumme.pl
@@ -2,36 +2,12 @@ use strict;
 use warnings;
 use functions;
 
-print &functions::quersumme(123456);
+print &functions::quersumme(123456), "\n";
 
 
 my @num=(1420,2004,3526,99999,80023,850,356,115,000001,999999);
 print "@num,\n";
 
-@num=sort{
-
-my $zahl=$a;
-my @ziffern=split(//,$zahl);
-my $l=@ziffern;
-my $erg=0;
-
-for(my $i=0;$i<$l;$i++)
-{
-    $erg = $erg+$ziffern[$i];
-}
-
-$zahl=$b;
-@ziffern=split(//,$zahl);
-$l=@ziffern;
-my $ergb=0;
-
-for(my $i=0;$i<$l;$i++)
-{
-    $ergb = $ergb+$ziffern[$i];
-}
-
-$erg<=>$ergb||$a<=>$b;
-
-}(@num);
+@num=sort{&functions::quersumme($a)<=>&functions::quersumme($b)||$a<=>$b;}(@num);
 
 print "@num\n";
