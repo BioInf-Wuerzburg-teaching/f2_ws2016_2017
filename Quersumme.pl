dea5d2af4c170228cc39122d72bd3be22a6a74d4
Mel Sch <melsch@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-28 10:47:07 +0200

    Nach Quersumme sortiert

--- Quersumme.pl
+++ Quersumme.pl
@@ -1,7 +1,7 @@
 use strict;
 use warnings;
 
-my $zahl=123456Z;
+my $zahl=123456;
 my @ziffern=split(//,$zahl);
 my $l=@ziffern;
 my $erg=0;
@@ -12,3 +12,35 @@ for(my $i=0;$i<$l;$i++)
 }
 
 print "$erg","\n";
+
+
+my @num=(115,2004,3526,99999,80023,850,356,1420,000001,999999);
+print "@num,\n";
+
+@num=sort{
+
+my $zahl=$a;
+my @ziffern=split(//,$zahl);
+my $l=@ziffern;
+my $erg=0;
+
+for(my $i=0;$i<$l;$i++)
+{
+    $erg = $erg+$ziffern[$i];
+}
+
+$zahl=$b;
+@ziffern=split(//,$zahl);
+$l=@ziffern;
+my $ergb=0;
+
+for(my $i=0;$i<$l;$i++)
+{
+    $ergb = $ergb+$ziffern[$i];
+}
+
+$erg<=>$ergb;
+
+}(@num);
+
+print "@num\n";
