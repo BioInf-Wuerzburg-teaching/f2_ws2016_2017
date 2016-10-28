51be9e09cb2d4bfc505a4d6581d2b457d3241259
Jocelyn Orf <jocorf@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-28 10:47:01 +0200

    quersumme und nach quersumme sortieren

--- quersumme.pl
+++ quersumme.pl
@@ -2,7 +2,9 @@ use strict;
 use warnings;
 
 
-my $zahl="123456";
+#quersumme
+
+my $zahl=123456;
 my @ziffern=split(//,$zahl);
 my $l=@ziffern;
 my $result=0;
@@ -12,4 +14,37 @@ for (my $i=0;$i<$l;$i++)
     $result=$result+$ziffern[$i];
 }
 
-print "$result\n";
+print "$result\n\n";
+
+
+#nach quersumme sortieren
+
+my @num=(115,2004,3526,99999,80023,850,356,1420,000001,999999);
+print "@num\n";
+
+@num=sort
+{
+    my $zahl=$a;
+    my @ziffern=split(//,$zahl);
+    my $l=@ziffern;
+    my $erg=0;
+
+    for (my $i=0;$i<$l;$i++)
+    {
+	$erg=$erg+$ziffern[$i];
+    }
+
+    $zahl=$b;
+    @ziffern=split(//,$zahl);
+    $l=@ziffern;
+    my $result=0;
+
+    for (my $i=0;$i<$l;$i++)
+    {
+	$result=$result+$ziffern[$i];
+    }
+
+    $erg<=>$result;
+}(@num);
+
+print "@num\n\n";
