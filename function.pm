e1decb0e69d37ddaeb5712481aa75f72dedce244
Florens Fischer <flofis@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-28 12:21:04 +0200

    function und quersumme mit funktionen

--- function.pm
+++ function.pm
@@ -1,14 +1,34 @@
-package functions;
+package function;
 use strict;
 use warnings;
 
 
 sub helloworld
 {
-    print "Hallo Welt! ";
+    return "Hallo Welt! ";
 }
 
+sub add
+{
+    my $S1=$_[0];
+    my $S2=$_[1];
+    return $S1 + $S2;
+    
+}
 
+sub quersumme
+{
+    my $var=$_[0];
+    my @reihe=split(//,$var);
+    my $quer=0;
+    my $l=@reihe;
+
+    for (my $i=0; $i<=$l-1; $i++)
+    {
+	$quer = $quer + $reihe[$i];   
+    }    
+    return $quer;
+}
 
 
 1;
--- quersumme.pl
+++ quersumme.pl
@@ -1,57 +1,17 @@
 use warnings;
 use strict;
-
-my $var=123456;
-my @reihe=split(//,$var);
-my $quer=0;
-my $l=@reihe;
-# for (my $i=0; $i<=$laengezahl; $i++)
-# {
-#     if($i<=$laengezahl)
-#     {$quersumme=$quersumme+$reihe[i];
-#     }
-    
-# }
-for (my $i=0; $i<=$l-1; $i++)
-{
-    $quer = $quer + $reihe[$i];   
-}    
-print $quer,"\n";
+use function;
 
 
-#my $quersumme= add $reihe[0..$laengezahl];
-#print $quersumme;
+print &function::quersumme(123456),"\n";
+
 
 my @num=(115,2004,3526,99999,80023,850,356,1420,000001,999999);
 print "@num\n";
 
-my @sortnum= sort{
-
-my $var=$a;
-my @reihe=split(//,$var);
-my $quer=0;
-my $l=@reihe;
-
-
-for (my $i=0; $i<=$l-1; $i++)
+my @sortnum= sort
 {
-    $quer = $quer + $reihe[$i];   
-}
-
-$var=$b;
-@reihe=split(//,$var);
-my $querb=0;
-$l=@reihe;
-
-
-for (my $i=0; $i<=$l-1; $i++)
-{
-    $querb = $querb + $reihe[$i];   
-}    
-
-$quer<=>$querb || $a<=>$b;
-
-
+    &function::quersumme($a)<=>&function::quersumme($b) || $a<=>$b;
 
 }(@num);
 
