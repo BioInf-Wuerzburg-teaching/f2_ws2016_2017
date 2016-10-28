f85a8bef6d8ab98aea6b676e73f2aac063ecc333
Anna-Sophie Schaller <Obsivergate@web.de>
2016-10-28 12:21:33 +0200

    quersumme verkürzt

--- quersumme.pl
+++ quersumme.pl
@@ -1,42 +1,11 @@
 use strict;
 use warnings;
+use functions;
 
-my $zahl=123456;
-my @ziffer=split(//,$zahl);
-my $l=@ziffer;
-my $erg=0;
-
-for(my $i=0;$i<$l;$i++)
-{
-    $erg = $erg+$ziffer[$i];
-}
-
-print "$erg\n";
+print &functions::quersumme(123456);
 
 my @num=(115,2004,3526,99999,80023,850,356,1420,000001,999999);
 print "@num\n";
 
-@num=sort{
-
-my $zahl=$a;
-my @ziffer=split(//,$zahl);
-my $l=@ziffer;
-my $erg=0;
-
-for(my $i=0;$i<$l;$i++)
-{
-    $erg = $erg+$ziffer[$i];
-}
-$zahl=$b;
-@ziffer=split(//,$zahl);
-$l=@ziffer;
-my $ergb=0;
-
-for(my $i=0;$i<$l;$i++)
-{
-    $ergb = $ergb+$ziffer[$i];
-}
-$erg<=>$ergb||$a<=>$b;
-
-}(@num);
+@num=sort{&functions::quersumme($a)<=>&functions::quersumme($b)||$a<=>$b;}(@num);
 print"@num\n";
