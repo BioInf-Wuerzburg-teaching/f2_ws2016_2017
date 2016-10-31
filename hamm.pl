2ab4d67909f4d10d6747059a7118babfc1708f08
Florens Fischer <flofis@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-31 11:05:11 +0100

    hamm

---
+++ hamm.pl
@@ -0,0 +1,30 @@
+use warnings;
+use strict;
+use function;
+
+
+open (BLA,"/home/flofis/dna1.txt") || die;
+
+my $dna1=<BLA>;
+my $dna2=<BLA>;
+close BLA || die;
+
+
+my @bases1=split(//,$dna1);
+my @bases2=split(//,$dna2);
+
+my $ende=@bases1;
+
+my $error = 0;
+
+for(my $i=0;$i<($ende-1);$i++)
+{
+    if($bases1[$i] ne $bases2[$i])
+    {
+	$error++;
+    }
+
+    
+}
+
+print "$error\n";
