d0a9ef7413f253dc76a05fad2f4b9c390d64152b
Florens Fischer <flofis@wbdw007.biozentrum.uni-wuerzburg.de>
2016-10-31 17:25:01 +0100

    read_fasta neuneu

--- read_fasta.pl
+++ read_fasta.pl
@@ -2,42 +2,22 @@ use warnings;
 use strict;
 use function;
 
-open (ZEILE,"/home/flofis/fasta.txt") || die;
-my @header;
-#my $dnastring=0;
+open (ZEILE,'<',"/home/flofis/fasta.txt") || die;
+my $header;
 my %hash;   
-my $zeile1;
 
-while($zeile1=<ZEILE>)
-{if($zeile1=~/>/)
+while(my $zeile1=<ZEILE>)
+{if($zeile1=~/^>(\S+)/)
  {
-    @header = split(/\s/,$zeile1);
-  #  if($dnastring)
- #   {
-#	print $dnastring,"\n";
-   # }
-    #print "\n",$header[0],"\n";
-
-   # $dnastring="";
+     $header =$1;
  }
-
  else
  {
      chomp $zeile1;
-     
-    # $dnastring=$dnastring.$zeile1;
-     $hash{$header[0]}.=$zeile1;
-     
+     $hash{$header}.=$zeile1; 
  }
-
 }
-#$hash{$header[0]}=$dnastring;
- # print $dnastring,"\n";
- 
-#%hash=%hash.($header[0]=>$dnastring);
-print %hash,"\n";
 
 close ZEILE || die;
-
 use Data::Dumper;
 print Dumper(\%hash);
