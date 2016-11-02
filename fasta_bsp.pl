f5dfa12ee01270cf98c6392f1b323a3ba21dcc4b
Anna-Sophie Schaller <Obsivergate@web.de>
2016-11-02 10:51:51 +0100

    fasta uebung mit funktion

--- fasta_bsp.pl
+++ fasta_bsp.pl
@@ -2,28 +2,9 @@ use warnings;
 use strict;
 use functions;
 
-open (FASTABSP,"<","fastaueben.txt") or die;
-my $header;
-
-my %hash;
-
-while(my $zeile1=<FASTABSP>)
-{
-    if($zeile1=~/^>(\S+)/)
-    {
-	$header = $1;	
-    }
-    else
-    {
-	chomp $zeile1;
-	$hash{$header}.=$zeile1;
-    }
-}
-
-
-print Dumper(\%hash);
-
-
-
-close FASTABSP or die;
 use Data::Dumper;
+
+my %hash=&functions::read_fasta("fastaueben.txt");
+
+print Dumper (\%hash);
+    
