b283b17aaddaa13b0d61e3acf2b803b363752adc
Jocelyn Orf <jocorf@wbdw007.biozentrum.uni-wuerzburg.de>
2016-11-04 11:34:57 +0100

    auf best position zugreifen

--- fastq_sample.pl
+++ fastq_sample.pl
@@ -5,4 +5,6 @@ use Data::Dumper;
 
 my $output=&functions::fastq("fastq_sample.txt");
 print ref($output),"\n\n";
-print Dumper($output);
+#print Dumper($output);
+print $output->{SEQ_ID2}->{"seq"}->[4],"\n";   #gib mir von dem ersten array, den ich abgespeichert habe das 5. element
+print $output->{SEQ_ID2}->{"quality"}->[4],"\n";
