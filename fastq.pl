c18016021cf9e720f8ca46880cdc6e0adf6ec6d3
Anna-Sophie Schaller <Obsivergate@web.de>
2016-11-04 11:34:48 +0100

    fastq code lesbarer

--- fastq.pl
+++ fastq.pl
@@ -6,6 +6,6 @@ use Data::Dumper;
 my $output=&functions::fastq("fastqwiki.txt");
 print ref($output),"\n";
 #print Dumper($output);
-print $output->{SEQ_ID1}->[0]->[5],"\n";
-print $output->{SEQ_ID1}->[1]->[5],"\n";
+print $output->{SEQ_ID1}->{"seq"}->[5],"\n";
+print $output->{SEQ_ID1}->{"quality"}->[5],"\n";
 
