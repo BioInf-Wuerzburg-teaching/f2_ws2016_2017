7aa7eba820a73ada40e882161f3777d412938a96
Anna-Sophie Schaller <Obsivergate@web.de>
2016-11-21 13:43:15 +0100

    rosalind5

---
+++ rosalind5.pl
@@ -0,0 +1,29 @@
+use warnings;
+use strict;
+
+
+open (BLA,"/home/annsch/dna1.txt") || die;
+
+my $dna1=<BLA>;
+my $dna2=<BLA>;
+close BLA || die;
+
+
+my @bases1=split(//,$dna1);
+my @bases2=split(//,$dna2);
+
+my $ende=@bases1;
+
+my $error = 0;
+
+for(my $i=0;$i<($ende-1);$i++)
+{
+    if($bases1[$i] ne $bases2[$i])
+    {
+	$error++;
+    }
+
+    
+}
+
+print "$error\n";
