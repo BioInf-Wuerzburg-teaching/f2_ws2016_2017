69f645e2f271aa356f4dbbd806223d1a2b29f449
Jocelyn Orf <jocorf@wbdw007.biozentrum.uni-wuerzburg.de>
2016-12-15 12:30:26 +0100

    ros10

---
+++ ros10.pl
@@ -0,0 +1,52 @@
+use strict;
+  use warnings;
+# use Data::Dump qw(dump);
+  
+ my $file = 'ros10.txt';
+  my $threshold = 0;
+  
+  open(FILE, '<', $file) || die $!;
+  my $counter = 0;
+  my $nextLineIsQualityLine = 0;
+
+ my @qualityLines;
+  my $qualityLineCounter = 0;
+  
+  while(<FILE>) {
+ chomp;
+     if($counter == 0) {
+         $threshold = $_;
+      } elsif($_ eq '+') {
+          $nextLineIsQualityLine = 1;
+      } elsif($nextLineIsQualityLine == 1) {
+        push(@qualityLines, $_);
+          $qualityLineCounter++;
+          $nextLineIsQualityLine = 0;
+      }
+      $counter++;
+  }
+  close FILE || die;
+  
+
+my $currentChar;
+my $finalQuality;
+my $colSum = 0;
+  my $badQualityCounter = 0;
+
+  
+
+while($qualityLines[0] ne "") {
+    for(my $row = 0; $row < @qualityLines; $row++) {
+        $currentChar = substr($qualityLines[$row], 0, 1);
+        $qualityLines[$row] =~ s/^.//s;
+        $finalQuality = ord($currentChar) - 33;
+        $colSum += $finalQuality;
+    }
+    if($colSum / $qualityLineCounter < $threshold) {
+          $badQualityCounter++;
+      }
+    $colSum = 0;
+  }
+  
+
+ print $badQualityCounter;
