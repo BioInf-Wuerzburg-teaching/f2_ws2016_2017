a2d6e6b486a55a44685e0c451f99bd947faa6d92
Florens Fischer <flofis@wbdw007.biozentrum.uni-wuerzburg.de>
2016-12-20 11:23:06 +0100

    ros10

---
+++ rosalind10.pl
@@ -0,0 +1,39 @@
+use strict;
+use warnings;
+
+my $file = 'ros10.txt';
+my $threshold = 0;
+
+open(FILE, '<', $file) || die $!;
+my $counter = 0;
+my $nextLineIsQualityLine = 0;
+my $qualityLines;
+my $qualityLineCounter = 0;
+
+while(<FILE>) {
+    chomp;
+    if($counter == 0) {
+        $threshold = $_;
+    } elsif($_ eq '+') {
+        $nextLineIsQualityLine = 1;
+    } elsif($nextLineIsQualityLine == 1) {
+        $qualityLines = $qualityLines.$_;
+        $qualityLineCounter++;
+        $nextLineIsQualityLine = 0;
+    }
+    $counter++;
+}
+close FILE || die;
+
+my @qualityLineArray = split(//, $qualityLines);
+my $badQualityCounter = 0;
+my $finalQuality = 0;
+
+for(my $i = 0; $i < @qualityLineArray; $i++) {
+    $finalQuality = ord($qualityLineArray[$i]) - 33;
+    if($finalQuality < $threshold) {
+        $badQualityCounter++;
+    }
+}
+
+print $badQualityCounter / $qualityLineCounter,"\n";
